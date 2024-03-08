5ded5c788e909486a5d30f8f0c0f1fc3713ef69c
Brandon Wright <brandonfullstack@gmail.com>
2024-03-08 10:25:14 -0500

    Temporarily make UserId nullable

--- MetricsApi/Migrations/20240308024515_userId_nullable.Designer.cs
+++ MetricsApi/Migrations/20240308024515_userId_nullable.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using MetricsApi.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace MetricsApi.Migrations
 {
     [DbContext(typeof(MetricsApiContext))]
-    partial class MetricsApiContextModelSnapshot : ModelSnapshot
+    [Migration("20240308024515_userId_nullable")]
+    partial class userId_nullable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- MetricsApi/Models/Metric.cs
+++ MetricsApi/Models/Metric.cs
@@ -5,7 +5,7 @@ namespace MetricsApi.Models
     public int MetricId { get; set; }
     public int Value { get; set; }
     public DateTime Date { get; set; }
-    public int UserId { get; set; }
+    public int? UserId { get; set; }
     public int PortalId { get; set; }
     public int InteractionId { get; set; }
   }
